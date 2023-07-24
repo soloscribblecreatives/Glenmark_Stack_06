4f53863bc226494cd681e7bebe66be5cc7d356e8
soloscribblecreatives <34957574+soloscribblecreatives@users.noreply.github.com>
2023-07-24 21:02:57 +0530

    Am Update

--- global.js
+++ global.js
@@ -388,19 +388,19 @@ switch(pg_id){
 	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="s1_1"><img src="slide1/s1_1.png"/></div><div class="s1_2"><img src="slide1/s1_2.png"/></div><div class="s1_3"><img src="slide1/s1_3.png"/></div><div class="s1_4"><img src="slide1/s1_4.png"/></div><div class="s1_5"><img src="slide1/s1_5.png"/></div>';
 	break;
 	case 2:
-	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="s2_1"><img src="slide2/s2_1.png"/></div><div class="wrap"><div class="s2_2"><img src="slide2/s2_2.png"/></div><div class="s2_3"><img src="slide2/s2_3.png"/></div><div class="s2_4"><img src="slide2/s2_4.png"/></div></div><div class="s2_5"><img src="slide2/s2_5.png"/></div><div class="s2_6"><img src="slide2/s2_6.png"/></div><div class="s2_7"><img src="slide2/s2_7.png"/></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="s2_1"><img src="slide2/s2_1.png"/></div><div class="wrap"><div class="s2_2"><img src="slide2/s2_2.png"/></div><div class="s2_3"><img src="slide2/s2_3.png"/></div><div class="s2_4"><img src="slide2/s2_4.png"/></div></div><div class="s2_5"><img src="slide2/s2_5.png"/></div><div class="s2_6"><img src="slide2/s2_6.png"/></div><div class="s2_7"><img src="slide2/s2_7.gif"/></div>';
 	break;
 	case 3:
 	content='<link rel="stylesheet" type="text/css" href="slide3/slide3.css" media="screen"/><div class="s3_1"><img src="slide3/s3_1.png" width="1024" height="768" alt=""></div><div class="s3_2"><img src="slide3/s3_2.png"/></div><div class="pop"><img src="slide3/pop.png" width="1024" height="768" alt=""/></div><div class="popUp" onclick="popUp()"></div><div class="closeIt" onclick="closeIt()"></div>';
 	break;
 	case 4:
-	content='<link rel="stylesheet" type="text/css" href="slide4/slide4.css" media="screen"/><div class="s4_1"><img src="slide4/s4_1.png" width="1024" height="768" alt=""></div><div class="s4_2"><img src="slide4/s4_2.png"/></div><div class="s4_3"><img src="slide4/s4_3.png"/></div><div class="s4_4"><img src="slide4/s4_4.png"/></div><div class="pop"><img src="slide4/pop.png" width="1024" height="768" alt=""/></div><div class="popUp" onclick="popUp()"></div><div class="closeIt" onclick="closeIt()"></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide4/slide4.css" media="screen"/><div class="s4_1"><img src="slide4/s4_1.png" width="1024" height="768" alt=""></div><div class="s4_2"><img src="slide4/s4_2.png"/></div><div class="s4_3"><img src="slide4/s4_3.png"/></div><div class="s4_4"><img src="slide4/s4_4.png"/></div><div class="pop"><img src="slide4/pop.png" width="1024" height="768" alt=""/></div><div class="popUp" onclick="popUp()"></div><div class="closeIt" onclick="closeIt()"></div><div class="graph"><img src="slide4/graph.png" width="1024" height="768" alt=""/></div><div class="graphUp" onclick="graphUp()"></div><div class="closeIt" onclick="closeIt()"></div>';
 	break;
 	case 5:
 	content='<link rel="stylesheet" type="text/css" href="slide5/slide5.css" media="screen"/><div class="s5_1"><img src="slide5/s5_1.png" width="1024" height="768" alt=""></div><div class="s5_2"><img src="slide5/s5_2.png"/></div><div class="s5_3"><img src="slide5/s5_3.png"/></div><div class="s5_4"><img src="slide5/s5_4.png"/></div><div class="pop"><img src="slide5/pop.png" width="1024" height="768" alt=""/></div><div class="popUp" onclick="popUp()"></div><div class="closeIt" onclick="closeIt()"></div>';
 	break;
 	case 6:
-	content='<link rel="stylesheet" type="text/css" href="slide6/slide6.css" media="screen"/><div class="s6_1"><img src="slide6/s6_1.png" width="1024" height="768" alt=""></div><div class="s6_2"><img src="slide6/s6_2.png"/></div><div class="s6_3"><img src="slide6/s6_3.png"/></div><div class="pop"><img src="slide6/pop.png" width="1024" height="768" alt=""/></div><div class="popUp" onclick="popUp()"></div><div class="closeIt" onclick="closeIt()"></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide6/slide6.css" media="screen"/><div class="s6_1"><img src="slide6/s6_1.png" width="1024" height="768" alt=""></div><div class="s6_2"><img src="slide6/s6_2.png"/></div><div class="s6_3"><img src="slide6/s6_3.png"/></div><div class="pop"><img src="slide6/pop.png" width="1024" height="768" alt=""/></div><div class="popUp" onclick="popUp()"></div><div class="closeIt" onclick="closeIt()"></div><div class="graph"><img src="slide6/graph.png" width="1024" height="768" alt=""/></div><div class="graphUp" onclick="graphUp()"></div><div class="closeIt" onclick="closeIt()"></div>';
 	break;
 	case 7:
 	content='<link rel="stylesheet" type="text/css" href="slide7/slide7.css" media="screen"/><div class="s7_1"><img src="slide7/s7_1.png" width="1024" height="768" alt=""></div><div class="s7_2"><img src="slide7/s7_2.png"/></div><div class="s7_3"><img src="slide7/s7_3.png"/></div><div class="s7_4"><img src="slide7/s7_4.png"/></div><div class="pop"><img src="slide7/pop.png" width="1024" height="768" alt=""/></div><div class="popUp" onclick="popUp()"></div><div class="closeIt" onclick="closeIt()"></div>';
@@ -567,10 +567,18 @@ function apiUp() {
 	$('.apiUp').css("display","none");
 }
 
+function graphUp() {
+	$('.graph').css("display","block");
+	$('.closeIt').css("display","block");
+	$('.graphUp').css("display","none");
+}
+
 function closeIt() {
 	$('.pop').css("display","none");
 	$('.api').css("display","none");
+	$('.graph').css("display","none");
 	$('.closeIt').css("display","none");
 	$('.popUp').css("display","block");
 	$('.apiUp').css("display","block");
+	$('.graphUp').css("display","block");
 }
